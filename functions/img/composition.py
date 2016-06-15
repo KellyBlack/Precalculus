1f8df101b9dfd67d9bdf6edaaa3c9ee1ba9e79fc
KellyBlack <kjblack@gmail.com>
2016-06-15 10:49:10 -0400

    Added a new activity on inverse functions

--- functions/img/composition.py
+++ functions/img/composition.py
@@ -24,7 +24,7 @@ plotter.setupGrid(0.3,'--',
                   -4.0,1.0,4.1)
 plotter.setAxesBounds(-4.1,4.1,-4.1,4.1)
 plotter.axesDecorations('Graph of f','x','f')
-plotter.addInterpolant([[-4,-2],[-2,0],[0,1],[2,2],[4,4]],
+plotter.addInterpolant([[-4,-2],[-3,-1],[-2,0],[0,1],[2,2],[4,4]],
                        np.arange(-4,4.01,0.1),'k-',2.5)
 
 
@@ -44,7 +44,7 @@ plotter.setupGrid(0.3,'--',
                   -4.0,1.0,4.1)
 plotter.setAxesBounds(-4.1,4.1,-4.1,4.1)
 plotter.axesDecorations('Graph of g','x','g')
-plotter.addInterpolant([[-4,3],[-2,1],[0,0],[2,-1],[4,-4]],
+plotter.addInterpolant([[-4,3],[-3,2],[-2,1],[0,0],[2,-1],[4,-4]],
                        np.arange(-4,4.01,0.1),'k-',2.5)
 
 
