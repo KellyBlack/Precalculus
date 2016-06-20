cbdd9fcbdc023e5abc1dca75e747055d8ee35032
KellyBlack <kjblack@gmail.com>
2016-06-20 12:39:05 -0400

    First draft of a python script to make an empty plot for the sine function

--- angles/img/axesEarth.py
+++ angles/img/axesEarth.py
@@ -19,11 +19,13 @@ plotter.clearPlot()
 #plt.xkcd(scale=.6) #randomness=1,length=1,scale=0)
 
 x = np.arange(0,5,0.02)
-plotter.setupGrid(0.3,'--',
-                  -5.0,1.0,5.1,
-                  -5.0,1.0,5.1)
-plotter.setAxesBounds(-5.1,5.1,-5.1,5.1)
-plotter.axesDecorations('Coordinate System','x','y')
+#plotter.setupGrid(0.3,'--',
+#                  0.0,np.pi/2,3.0*np.pi,
+#                  -1.0,1.0,1.1)
+plotter.setAxesBounds(0,8.1,-1.1,1.1)
+plotter.axesDecorations('Y-coordinate Of The Earth',r"$\theta$",'y')
+plotter.xAxisLabels(np.arange(0.0,3.0*np.pi,0.5*np.pi),
+                   ["0",r"$\frac{\pi}{2}$",r"$\pi$",r"$\frac{3\pi}{2}$",r"$2\pi$",r"$\frac{5\pi}{2}$"])
 
 axes = plotter.getAxes()
 axes.spines['right'].set_color('none')
@@ -33,32 +35,8 @@ axes.spines['bottom'].set_position(('data',0))
 axes.yaxis.set_ticks_position('left')
 axes.spines['left'].set_position(('data',0))
 axes.xaxis.set_label_coords(0.95, 0.45)
-axes.yaxis.set_label_coords(0.45, 0.95)
+axes.yaxis.set_label_coords(-0.05, 0.95)
 
-#plt.show()
-plt.savefig('emptyAxes.pgf',format='pgf')
-
-###############################
-plotter.clearPlot()
-#plt.xkcd(scale=.6) #randomness=1,length=1,scale=0)
-
-x = np.arange(0,5,0.02)
-plotter.setupGrid(0.3,'--',
-                  -5.0,1.0,5.1,
-                  -5.0,1.0,5.1)
-plotter.setAxesBounds(-5.1,5.1,-5.1,5.1)
-plotter.axesDecorations('Hare vs. Tortoise','Hare','Tortoise')
-
-axes = plotter.getAxes()
-axes.spines['right'].set_color('none')
-axes.spines['top'].set_color('none')
-axes.xaxis.set_ticks_position('bottom')
-axes.spines['bottom'].set_position(('data',0))
-axes.yaxis.set_ticks_position('left')
-axes.spines['left'].set_position(('data',0))
-axes.xaxis.set_label_coords(0.95, 0.45)
-axes.yaxis.set_label_coords(0.45, 0.90)
-
-#plt.show()
-plt.savefig('emptyAxesHareTortoise.pgf',format='pgf')
+plt.show()
+#plt.savefig('emptyAxesEarth.pgf',format='pgf')
 
