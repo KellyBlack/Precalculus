7d4f8b2b69a74c44aa8833d8f873f8b7746540da
KellyBlack <kjblack@gmail.com>
2016-06-20 12:16:17 -0400

    Initial copy of the python files to make an empty set of axes for the earth's movement

---
+++ angles/img/axesEarth.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.path as path
+from matplotlib.patches import FancyArrowPatch
+from matplotlib.patches import Ellipse
+import math
+
+import sys
+
+from BasicPlot import BasicPlot
+
+plotter = BasicPlot()
+plt.figure(num=1,frameon=False)
+
+###############################
+plotter.clearPlot()
+#plt.xkcd(scale=.6) #randomness=1,length=1,scale=0)
+
+x = np.arange(0,5,0.02)
+plotter.setupGrid(0.3,'--',
+                  -5.0,1.0,5.1,
+                  -5.0,1.0,5.1)
+plotter.setAxesBounds(-5.1,5.1,-5.1,5.1)
+plotter.axesDecorations('Coordinate System','x','y')
+
+axes = plotter.getAxes()
+axes.spines['right'].set_color('none')
+axes.spines['top'].set_color('none')
+axes.xaxis.set_ticks_position('bottom')
+axes.spines['bottom'].set_position(('data',0))
+axes.yaxis.set_ticks_position('left')
+axes.spines['left'].set_position(('data',0))
+axes.xaxis.set_label_coords(0.95, 0.45)
+axes.yaxis.set_label_coords(0.45, 0.95)
+
+#plt.show()
+plt.savefig('emptyAxes.pgf',format='pgf')
+
+###############################
+plotter.clearPlot()
+#plt.xkcd(scale=.6) #randomness=1,length=1,scale=0)
+
+x = np.arange(0,5,0.02)
+plotter.setupGrid(0.3,'--',
+                  -5.0,1.0,5.1,
+                  -5.0,1.0,5.1)
+plotter.setAxesBounds(-5.1,5.1,-5.1,5.1)
+plotter.axesDecorations('Hare vs. Tortoise','Hare','Tortoise')
+
+axes = plotter.getAxes()
+axes.spines['right'].set_color('none')
+axes.spines['top'].set_color('none')
+axes.xaxis.set_ticks_position('bottom')
+axes.spines['bottom'].set_position(('data',0))
+axes.yaxis.set_ticks_position('left')
+axes.spines['left'].set_position(('data',0))
+axes.xaxis.set_label_coords(0.95, 0.45)
+axes.yaxis.set_label_coords(0.45, 0.90)
+
+#plt.show()
+plt.savefig('emptyAxesHareTortoise.pgf',format='pgf')
+
