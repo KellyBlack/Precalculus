8c9fc63256010964fcddda702b9872174faf65d0
KellyBlack <kjblack@gmail.com>
2016-06-21 16:40:49 -0400

    Added a new activity on the sine and cosine functions a functions of a real number

--- trig/img/basicAxes.py
+++ trig/img/basicAxes.py
@@ -20,12 +20,14 @@ plotter.clearPlot()
 
 x = np.arange(0,5,0.02)
 plotter.setupGrid(0.3,'--',
-                  0.0,np.pi/2,3.0*np.pi,
+                  0.0,np.pi/2,6.0*np.pi,
                   -1.0,1.0,1.1)
-plotter.setAxesBounds(0,8.1,-1.1,1.1)
+plotter.setAxesBounds(0,19.0,-1.1,1.1)
 plotter.axesDecorations('Sine',r"$\theta$",'sin')
-plotter.xAxisLabels(np.arange(0.0,3.0*np.pi,0.5*np.pi),
-                   ["0",r"$\frac{\pi}{2}$",r"$\pi$",r"$\frac{3\pi}{2}$",r"$2\pi$",r"$\frac{5\pi}{2}$"])
+plotter.xAxisLabels(np.arange(0.0,6.0*np.pi+0.5,0.5*np.pi),
+                   ["0",r"$\frac{\pi}{2}$",r"$\pi$",r"$\frac{3\pi}{2}$",
+                    r"$2\pi$",r"$\frac{5\pi}{2}$",r"$3\pi$",r"$\frac{7\pi}{2}$",
+                    r"$4\pi$",r"$\frac{9\pi}{2}$",r"$5\pi$",r"$\frac{11\pi}{2}$",r"$6\pi$"])
 
 axes = plotter.getAxes()
 axes.spines['right'].set_color('none')
@@ -34,7 +36,7 @@ axes.xaxis.set_ticks_position('bottom')
 axes.spines['bottom'].set_position(('data',0))
 axes.yaxis.set_ticks_position('left')
 axes.spines['left'].set_position(('data',0))
-axes.xaxis.set_label_coords(0.95, 0.45)
+axes.xaxis.set_label_coords(0.95, 0.53)
 axes.yaxis.set_label_coords(-0.05, 0.95)
 
 #plt.show()
@@ -46,12 +48,14 @@ plotter.clearPlot()
 
 x = np.arange(0,5,0.02)
 plotter.setupGrid(0.3,'--',
-                  0.0,np.pi/2,3.0*np.pi,
+                  0.0,np.pi/2,6.0*np.pi,
                   -1.0,1.0,1.1)
-plotter.setAxesBounds(0,8.1,-1.1,1.1)
+plotter.setAxesBounds(0,19.1,-1.1,1.1)
 plotter.axesDecorations('Cosine',r"$\theta$",'cos')
-plotter.xAxisLabels(np.arange(0.0,3.0*np.pi,0.5*np.pi),
-                   ["0",r"$\frac{\pi}{2}$",r"$\pi$",r"$\frac{3\pi}{2}$",r"$2\pi$",r"$\frac{5\pi}{2}$"])
+plotter.xAxisLabels(np.arange(0.0,6.0*np.pi+0.5,0.5*np.pi),
+                   ["0",r"$\frac{\pi}{2}$",r"$\pi$",r"$\frac{3\pi}{2}$",
+                    r"$2\pi$",r"$\frac{5\pi}{2}$",r"$3\pi$",r"$\frac{7\pi}{2}$",
+                    r"$4\pi$",r"$\frac{9\pi}{2}$",r"$5\pi$",r"$\frac{11\pi}{2}$",r"$6\pi$"])
 
 axes = plotter.getAxes()
 axes.spines['right'].set_color('none')
@@ -60,7 +64,7 @@ axes.xaxis.set_ticks_position('bottom')
 axes.spines['bottom'].set_position(('data',0))
 axes.yaxis.set_ticks_position('left')
 axes.spines['left'].set_position(('data',0))
-axes.xaxis.set_label_coords(0.95, 0.45)
+axes.xaxis.set_label_coords(0.95, 0.53)
 axes.yaxis.set_label_coords(-0.05, 0.95)
 
 #plt.show()
