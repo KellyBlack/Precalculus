bd54368a4f59c9566a0f327b813c44b629c291a1
KellyBlack <kjblack@gmail.com>
2016-06-21 16:15:11 -0400

    Fixed the python script to generate an image with empty axes

--- trig/img/basicAxes.py
+++ trig/img/basicAxes.py
@@ -1,140 +1,68 @@
 #!/usr/bin/python
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.path as path
+from matplotlib.patches import FancyArrowPatch
+from matplotlib.patches import Ellipse
+import math
 
+import sys
 
+from BasicPlot import BasicPlot
 
-class BasicPlot:
+plotter = BasicPlot()
+plt.figure(num=1,frameon=False)
 
-    def __init__(self):
-        plt.rcParams['axes.unicode_minus']=False
-        #plt.figure(num=1,frameon=False)
-        plt.rcParams['text.usetex'] = True
-        plt.rcParams['text.latex.unicode'] = True
-        self.clearPlot()
+###############################
+plotter.clearPlot()
+#plt.xkcd(scale=.6) #randomness=1,length=1,scale=0)
 
+x = np.arange(0,5,0.02)
+plotter.setupGrid(0.3,'--',
+                  0.0,np.pi/2,3.0*np.pi,
+                  -1.0,1.0,1.1)
+plotter.setAxesBounds(0,8.1,-1.1,1.1)
+plotter.axesDecorations('Sine',r"$\theta$",'sin')
+plotter.xAxisLabels(np.arange(0.0,3.0*np.pi,0.5*np.pi),
+                   ["0",r"$\frac{\pi}{2}$",r"$\pi$",r"$\frac{3\pi}{2}$",r"$2\pi$",r"$\frac{5\pi}{2}$"])
 
-    def clearPlot(self):
-        plt.clf()
+axes = plotter.getAxes()
+axes.spines['right'].set_color('none')
+axes.spines['top'].set_color('none')
+axes.xaxis.set_ticks_position('bottom')
+axes.spines['bottom'].set_position(('data',0))
+axes.yaxis.set_ticks_position('left')
+axes.spines['left'].set_position(('data',0))
+axes.xaxis.set_label_coords(0.95, 0.45)
+axes.yaxis.set_label_coords(-0.05, 0.95)
 
-    def setFigure(self,number=None,aspectRatio=(1,1),
-                  dotsPerInch=80,face='w',edge='k'):
-        return(plt.figure(num=number, figsize=aspectRatio,
-                          dpi=dotsPerInch, facecolor=face, edgecolor=edge))
+#plt.show()
+plt.savefig('emptySineAxes.pgf',format='pgf')
 
-    def addFunction(self,domain,range,format,width=2.0):
-        plt.plot(domain,range,format,linewidth=width)
+###############################
+plotter.clearPlot()
+#plt.xkcd(scale=.6) #randomness=1,length=1,scale=0)
 
-    def setAxesBounds(self,lowx,highx,lowy,highy):
-        plt.xlim([lowx,highx])
-        plt.ylim([lowy,highy])
+x = np.arange(0,5,0.02)
+plotter.setupGrid(0.3,'--',
+                  0.0,np.pi/2,3.0*np.pi,
+                  -1.0,1.0,1.1)
+plotter.setAxesBounds(0,8.1,-1.1,1.1)
+plotter.axesDecorations('Cosine',r"$\theta$",'cos')
+plotter.xAxisLabels(np.arange(0.0,3.0*np.pi,0.5*np.pi),
+                   ["0",r"$\frac{\pi}{2}$",r"$\pi$",r"$\frac{3\pi}{2}$",r"$2\pi$",r"$\frac{5\pi}{2}$"])
 
-    def setupGrid(self,thickness=1.0,style='--',
-                  startx=0.0,deltax=0.1,endx=1.0,
-                  starty=0.0,deltay=0.1,endy=1.0):
+axes = plotter.getAxes()
+axes.spines['right'].set_color('none')
+axes.spines['top'].set_color('none')
+axes.xaxis.set_ticks_position('bottom')
+axes.spines['bottom'].set_position(('data',0))
+axes.yaxis.set_ticks_position('left')
+axes.spines['left'].set_position(('data',0))
+axes.xaxis.set_label_coords(0.95, 0.45)
+axes.yaxis.set_label_coords(-0.05, 0.95)
 
-        ax = self.getAxes()
-        xticklines = plt.getp(ax, 'xticklines')
-        yticklines = plt.getp(ax, 'yticklines')
-        xgridlines = plt.getp(ax, 'xgridlines')
-        ygridlines = plt.getp(ax, 'ygridlines')
-        xticklabels = plt.getp(ax, 'xticklabels')
-        yticklabels = plt.getp(ax, 'yticklabels')
-
-        plt.setp(xticklines, 'linewidth', thickness)
-        plt.setp(yticklines, 'linewidth', thickness)
-        plt.setp(xgridlines, 'linestyle', style)
-        plt.setp(ygridlines, 'linestyle', style)
-        plt.setp(yticklabels, 'color', 'k', fontsize='medium')
-        plt.setp(xticklabels, 'color', 'k', fontsize='medium')
-        ax.set_xticks(np.arange(startx,endx,deltax))
-        ax.set_yticks(np.arange(starty,endy,deltay))
-
-        ax.xaxis.grid(linewidth=thickness)
-        ax.yaxis.grid(linewidth=thickness)
-        plt.grid(True)
-
-    def yAxisLabels(self,positions,labels,rot="horizontal",margins=0.2):
-        plt.yticks(positions,labels,rotation=rot)
-        plt.margins(margins)
-
-    def xAxisLabels(self,positions,labels,rot="horizontal",margins=0.2):
-        ax = self.getAxes()
-        ax.set_xticks(positions)
-        ax.set_xticklabels(labels,rotation=rot)
-        plt.margins(margins)
-
-
-    def axesDecorations(self,title,horiztonalLabel,verticallabel):
-
-        plt.xlabel(horiztonalLabel)
-        plt.ylabel(verticallabel)
-        plt.title(title)
-
-    def placeText(self,xpos,ypos,text,**kwargs):
-        plt.text(xpos,ypos,text,kwargs)
-
-    def clearPlot(self):
-        plt.clf()
-
-    def markJumps(self,jumps,symbolSize):
-        for jump in jumps:
-            if(jump[2]):
-                plt.plot(jump[0],jump[1],'ok',markersize=symbolSize,mfc='black')
-            else:
-                plt.plot(jump[0],jump[1],'ok',markersize=symbolSize,mfc='white')
-
-    def subplot(self,row,col,number):
-        plt.subplot(row,col,number)
-
-    def addInterpolant(self,interpolants,t,format,width=2.0):
-        function = 0*t;
-        for point in interpolants:
-            currentSpline = 0*t + 1.0
-            for spline in interpolants:
-                if(spline != point):
-                    currentSpline *= (t-spline[0])/(point[0]-spline[0])
-            function += point[1]*currentSpline
-        self.addFunction(t,function,format,width)
-
-        
-    def getAxes(self):
-        return(plt.gca())
-
-    def setAxes(self,value):
-        if(value):
-            plt.axis('on')
-        else:
-            plt.axis('off')
-
-    def setAxesValue(self,value):
-        plt.axis(value)
-
-    def filledCircle(self,xcenter,ycenter,radius,color='k'):
-        figure = plt.gcf()
-        circle = plt.Circle((xcenter,ycenter),radius,color=color)
-        figure.gca().add_artist(circle)
-
-
-if (__name__ =='__main__') :
-    plotter = BasicPlot()
-
-    #plt.xkcd(randomness=0.1)
-    #with plt.xkcd():
-    plotter.clearPlot()
-
-    with plt.xkcd():
-        plotter.addFunction([0.0,1.0,2.0,3.0],[0.0,-1.0,2.0,1.0], 'k-',2.0)
-        plotter.addInterpolant([[0.0,0.0],[1.0,-1.0],[2.0,2.0],[3.0,1.0]],
-                               np.arange(0.0,3.1,0.1),'r--',1.0)
-        plotter.setupGrid(0.5,'--',
-                          0.0,1.0,3.1,
-                         -2.0,0.5,4.1,)
-        plotter.axesDecorations('Position of an Object','Time (sec)','Position (m)')
-        #plt.text(1.0,3.025, r'$\alpha=100,\ \sigma=15$'
-
-
-    plotter.setAxesBounds(0.0,3.05,-2.05,4.05)
-    plt.show()
-    #plt.savefig('preClass3.pgf')
+#plt.show()
+plt.savefig('emptyAxesCosine.pgf',format='pgf')
 
