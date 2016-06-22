adb5e7ab7aca33fad0ef995ac833af9cb2cdfb04
KellyBlack <kjblack@gmail.com>
2016-06-22 16:13:34 -0400

    Added an activity on graphing trig functions

--- trig/img/graphsOfTrig.py
+++ trig/img/graphsOfTrig.py
@@ -19,7 +19,6 @@ plotter.setFigure(None,(10,3.5))
 plotter.clearPlot()
 #plt.xkcd(scale=.6) #randomness=1,length=1,scale=0)
 
-x = np.arange(0,5,0.02)
 plotter.setupGrid(0.3,'--',
                   -3.0*np.pi,np.pi/2,3.0*np.pi,
                   -3.0,1.0,3.1)
@@ -49,11 +48,11 @@ plt.savefig('trigGraphs_1.pgf',format='pgf')
 
 
 ###############################
+plt.close('all')
 plotter.setFigure(None,(10,3.5))
 plotter.clearPlot()
 #plt.xkcd(scale=.6) #randomness=1,length=1,scale=0)
 
-x = np.arange(0,5,0.02)
 plotter.setupGrid(0.3,'--',
                   -3.0*np.pi,np.pi/2,3.0*np.pi,
                   -3.0,1.0,5.1)
@@ -82,11 +81,11 @@ axes.yaxis.set_label_coords(0.45, 0.95)
 plt.savefig('trigGraphs_2.pgf',format='pgf')
 
 ###############################
+plt.close('all')
 plotter.setFigure(None,(10,3.5))
 plotter.clearPlot()
 #plt.xkcd(scale=.6) #randomness=1,length=1,scale=0)
 
-x = np.arange(0,5,0.02)
 plotter.setupGrid(0.3,'--',
                   -3.0,0.5,3.0,
                   -2.0,1.0,5.1)
@@ -107,4 +106,58 @@ axes.yaxis.set_label_coords(0.45, 0.95)
 
 #plt.show()
 plt.savefig('trigGraphs_3.pgf',format='pgf')
+
+###############################
+plt.close('all')
+plotter.setFigure(None,(10,3.5))
+plotter.clearPlot()
+#plt.xkcd(scale=.6) #randomness=1,length=1,scale=0)
+
+plotter.setupGrid(0.3,'--',
+                  -4.0,1.0,4.1,
+                  -2.0,1.0,3.1)
+plotter.setAxesBounds(-4.1,4.1,-2.1,3.1)
+plotter.axesDecorations('Shifted Function',r"$x$",'D')
+#plotter.xAxisLabels(np.arange(-4.0,4.1,1.0),np.arange(-2.0,3.1,1.0))
+#plotter.setAspectRation(.8)
+
+axes = plotter.getAxes()
+axes.spines['right'].set_color('none')
+axes.spines['top'].set_color('none')
+axes.xaxis.set_ticks_position('bottom')
+axes.spines['bottom'].set_position(('data',0))
+axes.yaxis.set_ticks_position('left')
+axes.spines['left'].set_position(('data',0))
+axes.xaxis.set_label_coords(0.95, 0.45)
+axes.yaxis.set_label_coords(0.45, 0.95)
+
+#plt.show()
+plt.savefig('trigGraphs_4.pgf',format='pgf')
+
+###############################
+plt.close('all')
+plotter.setFigure(None,(10,3.5))
+plotter.clearPlot()
+#plt.xkcd(scale=.6) #randomness=1,length=1,scale=0)
+
+plotter.setupGrid(0.3,'--',
+                  -4.0,1.0,4.1,
+                  -5.0,1.0,1.6)
+plotter.setAxesBounds(-4.1,4.1,-5.1,1.6)
+plotter.axesDecorations('Shifted Function',r"$x$",'L')
+#plotter.xAxisLabels(np.arange(-4.0,4.1,1.0),np.arange(-2.0,3.1,1.0))
+#plotter.setAspectRation(.8)
+
+axes = plotter.getAxes()
+axes.spines['right'].set_color('none')
+axes.spines['top'].set_color('none')
+axes.xaxis.set_ticks_position('bottom')
+axes.spines['bottom'].set_position(('data',0))
+axes.yaxis.set_ticks_position('left')
+axes.spines['left'].set_position(('data',0))
+axes.xaxis.set_label_coords(0.95, 0.82)
+axes.yaxis.set_label_coords(0.45, 0.95)
+
+#plt.show()
+plt.savefig('trigGraphs_5.pgf',format='pgf')
 
