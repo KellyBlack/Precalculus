f1f4fbf1b86c9a832ec049c0af813eb694122f16
KellyBlack <kjblack@gmail.com>
2016-06-23 12:02:50 -0400

    Added a new plot of a blank axis to motivate the inverse sine

--- trig/img/graphsOfTrig.py
+++ trig/img/graphsOfTrig.py
@@ -161,3 +161,35 @@ axes.yaxis.set_label_coords(0.45, 0.95)
 #plt.show()
 plt.savefig('trigGraphs_5.pgf',format='pgf')
 
+###############################
+plt.close('all')
+plotter.setFigure(None,(10,3.5))
+plotter.clearPlot()
+#plt.xkcd(scale=.6) #randomness=1,length=1,scale=0)
+
+plotter.setupGrid(0.3,'--',
+                  -3.0*np.pi,np.pi/2,3.0*np.pi,
+                  -2.0,1.0,2.1)
+plotter.setAxesBounds(-9.5,9.5,-2.1,2.1)
+plotter.axesDecorations('Sine',r"$x$",'sine')
+plotter.xAxisLabels(np.arange(-3.0*np.pi,3.0*np.pi+0.5,0.5*np.pi),
+                   [r"$-3\pi$",r"$-\frac{5\pi}{2}$",
+                    r"$-2\pi$",r"$-\frac{3\pi}{2}$",
+                    r"$-\pi$",r"$-\frac{\pi}{2}$",
+                    "0",r"$\frac{\pi}{2}$",
+                    r"$\pi$",r"$\frac{3\pi}{2}$",
+                    r"$2\pi$",r"$\frac{5\pi}{2}$",r"$3\pi$"])
+#plotter.setAspectRation(.8)
+
+axes = plotter.getAxes()
+axes.spines['right'].set_color('none')
+axes.spines['top'].set_color('none')
+axes.xaxis.set_ticks_position('bottom')
+axes.spines['bottom'].set_position(('data',0))
+axes.yaxis.set_ticks_position('left')
+axes.spines['left'].set_position(('data',0))
+axes.xaxis.set_label_coords(0.95, 0.53)
+axes.yaxis.set_label_coords(0.45, 0.95)
+
+#plt.show()
+plt.savefig('blankAxesCenteredZero.pgf',format='pgf')
