6dca951b505b34a08ed1007c007dd2633948e083
KellyBlack <kjblack@gmail.com>
2016-06-24 17:05:02 -0400

    Added an image for the average rate of change

--- exponentials/img/avgRateChange.py
+++ exponentials/img/avgRateChange.py
@@ -17,36 +17,23 @@ plt.figure(num=1,frameon=False)
 ###############################
 plotter.clearPlot()
 #plt.xkcd(scale=.6) #randomness=1,length=1,scale=0)
-plotter.subplot(1,2,1)
+#plotter.subplot(1,2,1)
 
 plotter.setupGrid(0.3,'--',
                   -4.0,1.0,4.1,
                   -4.0,1.0,4.1)
 plotter.setAxesBounds(-4.1,4.1,-4.1,4.1)
 plotter.axesDecorations('Graph of f','x','f')
-plotter.addInterpolant([[-4,-2],[-3,-1],[-2,0],[0,1],[2,2],[4,4]],
-                       np.arange(-4,4.01,0.1),'k-',2.5)
+#plotter.addInterpolant([[-4,-2],[-3,-1],[-2,0],[0,1],[2,2],[4,4]],
+#                       np.arange(-4,4.01,0.1),'k-',2.5)
 
-
-axes = plotter.getAxes()
-axes.spines['right'].set_color('none')
-axes.spines['top'].set_color('none')
-axes.xaxis.set_ticks_position('bottom')
-axes.spines['bottom'].set_position(('data',0))
-axes.yaxis.set_ticks_position('left')
-axes.spines['left'].set_position(('data',0))
-axes.xaxis.set_label_coords(0.95, 0.45)
-axes.yaxis.set_label_coords(0.45, 0.95)
-
-plotter.subplot(1,2,2)
-plotter.setupGrid(0.3,'--',
-                  -4.0,1.0,4.1,
-                  -4.0,1.0,4.1)
-plotter.setAxesBounds(-4.1,4.1,-4.1,4.1)
-plotter.axesDecorations('Graph of g','x','g')
-plotter.addInterpolant([[-4,3],[-3,2],[-2,1],[0,0],[2,-1],[4,-4]],
-                       np.arange(-4,4.01,0.1),'k-',2.5)
-
+x = np.arange(-4.0,4.1,0.1)
+y = np.exp(x/3)
+plotter.addFunction(x,y,'k')
+slope = (np.exp(1.0)-1.0)/3.0
+plotter.filledCircle(0.0,1.0,0.1)
+plotter.filledCircle(3.0,np.exp(1.0),0.1)
+plotter.addFunction([-4.0,4.0],slope*np.arange(-4.0,4.1,8.0)+1.0,'k--')
 
 axes = plotter.getAxes()
 axes.spines['right'].set_color('none')
@@ -57,7 +44,8 @@ axes.yaxis.set_ticks_position('left')
 axes.spines['left'].set_position(('data',0))
 axes.xaxis.set_label_coords(0.95, 0.45)
 axes.yaxis.set_label_coords(0.45, 0.95)
+
 
 #plt.show()
-plt.savefig('composition.pgf',format='pgf')
+plt.savefig('avgRateChange.pgf',format='pgf')
 
